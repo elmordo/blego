63e06f9ba0230183bc7986fadc50b529b36e2ea2
Petr Jindra <el.mordo@gmail.com>
2025-04-15 15:36:37 +0200

    basic implementation of the HubProperties message

--- src/blego/lwp3/messages.py
+++ src/blego/lwp3/messages.py
@@ -23,8 +23,9 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
+from typing import Self
 
-from .enums import MessageType
+from .enums import MessageType, HubPropertyReference, HubPropertyOperation
 
 
 class Message(ABC):
@@ -32,6 +33,12 @@ class Message(ABC):
     @abstractmethod
     def get_message_type(self) -> MessageType:
         pass
+
+    @classmethod
+    @abstractmethod
+    def from_bytes(cls, data: bytes) -> Self:
+        """The message content without the message header (the first three bytes)"""
+        raise NotImplementedError()
 
 
 class HubMessage(Message, ABC):
@@ -42,11 +49,51 @@ class HubMessage(Message, ABC):
 
 
 class HubProperties(HubMessage):
+    """
+
+    Up+Down
+    """
+
+    MAX_NAME_SIZE = 14
+
+    def __init__(
+        self,
+        prop: HubPropertyReference,
+        op: HubPropertyOperation,
+        payload: bytes = b"",
+    ):
+        self.property = prop
+        self.operation = op
+        self.payload = payload
+
+    @classmethod
+    def from_bytes(cls, data: bytes) -> Self:
+        return cls(
+            prop=HubPropertyReference(data[0]),
+            op=HubPropertyOperation(data[1]),
+            payload=data[2:],
+        )
+
+    @classmethod
+    def new_set_advertising_name(cls, name: str) -> Self:
+        payload = name.encode("utf-8")
+
+        if len(payload) > cls.MAX_NAME_SIZE:
+            raise ValueError(
+                f"Max length of name is {cls.MAX_NAME_SIZE} bytes. Got {len(payload)} bytes instead."
+            )
+
+        return cls(
+            prop=HubPropertyReference.ADVERTISING_NAME,
+            op=HubPropertyOperation.SET,
+            payload=payload,
+        )
+
     def get_message_type(self) -> MessageType:
         return MessageType.HUB_PROPERTIES
 
     def encode(self) -> bytes:
-        raise NotImplementedError()
+        return bytes([self.property.value, self.operation.value]) + self.payload
 
 
 class HubActions(HubMessage):
