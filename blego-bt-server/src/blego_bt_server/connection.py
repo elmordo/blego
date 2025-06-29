272d20daa9cdb6b2e75cdecae294ebad1c557875
Petr Jindra <el.mordo@gmail.com>
2025-06-29 23:12:20 +0200

    connecting to lego hub

--- blego-bt-server/src/blego_bt_server/connection.py
+++ blego-bt-server/src/blego_bt_server/connection.py
@@ -19,8 +19,15 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
+from __future__ import annotations
+
+from enum import Enum
+
 from bleak import BleakClient
 
+from blego.hub import Port
+from blego_bt_server.constants import LEGO_SERVICE_UUID, LEGO_CHARACTERISTIC_UUID
 from blego_bt_server.scanner import AdvertisedHub
 
 
@@ -31,7 +38,7 @@ class ConnectedHub:
     def __init__(self, name: str, client: BleakClient):
         self._name = name
         self._client = client
-        self._ports = []
+        self._ports = {}
         self.__connected = False
 
     @classmethod
@@ -47,6 +54,7 @@ class ConnectedHub:
 
     async def connect(self):
         await self._client.connect()
+        await self._client.start_notify(LEGO_CHARACTERISTIC_UUID, self._handle_notification)
         self.__connected = True
 
     async def disconnect(self):
@@ -59,3 +67,28 @@ class ConnectedHub:
     @property
     def name(self) -> str:
         return self._name
+
+    def _handle_notification(self, sender, data):
+        if data[2] == 0x04:
+            # attach/detach of device
+            port_id = data[3]
+            operation = DeviceOperationType(data[4])
+            device_type_id = data[5]
+            self._handle_device_change(port_id, operation, device_type_id)
+        print(data[2], data[3], data[4], data[5])
+
+    def _handle_device_change(self, port_id: int, operation: DeviceOperationType, device_type_id: int):
+        if operation is DeviceOperationType.DETACH:
+            try:
+                self._ports.pop(port_id)
+            except KeyError:
+                # TODO: Log
+                pass
+        else:
+            self._ports[port_id] = device_type_id
+
+
+class DeviceOperationType(Enum):
+    DETACH = 0x00
+    ATTACH = 0x01
+    ATTACH_VIRTUAL = 0x02
